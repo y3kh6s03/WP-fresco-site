af565e9961017298eb147ff01d34736ec95038d0
yosuke026 <y3kh6s03@gmail.com>
2023-10-28 09:05:54 +0900

    workflow

--- js/main.js
+++ js/main.js
@@ -5,29 +5,29 @@ const navIcon = document.querySelector('.header__icon');
 const navMenu = document.querySelector('.header__nav');
 const navBackground = document.querySelector('.header__bcc');
 
-const toggleOnClick = ()=>{
+const toggleOnClick = () => {
     navIcon.classList.toggle("onClick");
     navMenu.classList.toggle("onClick");
     navBackground.classList.toggle("onClick");
 }
-navIcon.addEventListener("click",toggleOnClick)
-navMenu.addEventListener("click",toggleOnClick)
-navBackground.addEventListener("click",toggleOnClick)
+navIcon.addEventListener("click", toggleOnClick)
+navMenu.addEventListener("click", toggleOnClick)
+navBackground.addEventListener("click", toggleOnClick)
 
 
 
 //////////////////////////////
 // hero title animation
 //////////////////////////////
-window.addEventListener('load', function() {
+window.addEventListener('DOMContentLoaded', function () {
     const heroTitle = document.querySelector(".hero__title");
     heroTitle.classList.add("loaded");
 
     const navItems = document.querySelectorAll(".hero__nav__ul__li");
-    gsap.to(navItems,{
+    gsap.to(navItems, {
         x: 0,
         opacity: 1,
-        delay: 1.8,
+        delay: 1.5,
         duration: .3,
         ease: "expo.out",
         stagger: .1
@@ -35,9 +35,75 @@ window.addEventListener('load', function() {
 });
 
 
+//////////////////////////////
+// hero section animation
+//////////////////////////////
+const hero = document.querySelector(".hero");
+const heroContainer = document.querySelector(".hero__container");
+const aboutContainer = document.querySelector(".about");
+gsap.timeline({
+    scrollTrigger:{
+        trigger: hero,
+        start: "top top",
+        end: "bottom 30%",
+        scrub: true,
+        pin: true
+    }
+})
+.to(heroContainer,{y: "-100%"})
+.to(hero,{
+    // y:"-100%",
+    opacity: 0,
+}, "-=.4")
+//////////////////////////////
+// about scrub animation
+//////////////////////////////
+
+const imgEl1 = document.querySelector(".about__image__container__img1")
+const imgEl2 = document.querySelector(".about__image__container__img2")
+gsap.registerPlugin(ScrollTrigger);
+gsap.to(imgEl1, {
+    y: 30,
+    scrollTrigger: {
+        trigger: imgEl1,
+        start: "top bottom",
+        scrub: true,
+    }
+});
+gsap.to(imgEl2,{
+    x: 40,
+    scrollTrigger: {
+        trigger: imgEl1,
+        start: "top bottom",
+        scrub: true,
+    }
+})
+
+
+
+
 
 //////////////////////////////
-// works swiper Animation
+// works swiper animation
+//////////////////////////////
+
+window.addEventListener("DOMContentLoaded", () => {
+    gsap.utils.toArray(".content__title__container").forEach((el) => {
+        const selectEl = gsap.utils.selector(el);
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: el,
+                start: "top 100%",
+                toggleClass: "active",
+                toggleActions: "play none none reset",
+            },
+        }).to(selectEl(".content__title"), {})
+    })
+})
+
+//////////////////////////////
+// works swiper animation
 //////////////////////////////
 
 const swiper = new Swiper('.swiper', {
@@ -48,7 +114,7 @@ const swiper = new Swiper('.swiper', {
     speed: 5000,
     slidesPerView: 1,
 
-    breakpoints:{
+    breakpoints: {
         600: {
             slidesPerView: 1.5
         },
@@ -56,4 +122,4 @@ const swiper = new Swiper('.swiper', {
             slidesPerView: 1.8
         }
     }
-    });
+});
