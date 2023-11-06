8cc4a70f6a5ba8551ed5f6fadd2fe8cd3582a057
yosuke026 <y3kh6s03@gmail.com>
2023-11-06 17:33:59 +0900

    contact

--- page-contact.php
+++ page-contact.php
@@ -3,16 +3,17 @@
 ?>
 
 <?php get_header(); ?>
-    <section class="subHero" id="hero">
-        <div class="subHero__title">
-            <div class="subHero__title__inner">
-                <h1 class="subHero__title__h1">
-                    CONTACT</h1></div><span class="subHero__title__bar"></span><span class="subHero__title__ja">お問い合せ</span>
-        </div>
-        <div class="subHero__image">
-            <img src="<?php echo get_template_directory_uri();?>/images/contact-images/contact.jpg" alt="contact-image" class="subHero__image__img">
-        </div>
-    </section>
+<section class="subHero" id="hero">
+    <div class="subHero__title">
+        <div class="subHero__title__inner">
+            <h1 class="subHero__title__h1">
+                CONTACT</h1>
+        </div><span class="subHero__title__bar"></span><span class="subHero__title__ja">お問い合せ</span>
+    </div>
+    <div class="subHero__image">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/service-images/serviceHero.jpg" alt="contact-image" class="subHero__image__img">
+    </div>
+</section>
 <?php the_content(); ?>
 
 <?php get_footer(); ?>
